eb931a9abebdbaca6d0e8923575f6fdfa96afdb3
Victor <160278295+VictoryMa8@users.noreply.github.com>
2024-12-27 23:46:08 -0600

    New function

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+def square(x):
+    return x * x
+
 def main():
     print("Hello World")
     name = input("Enter your name: ")
-    print("Greetings, " + name)
+    num1 = input("Enter a number: ")
+    result = square(int(num1))
+    print("Greetings, " + name + ", " + num1 + " squared is " + str(result))
 
 if __name__ == "__main__":
     main()
