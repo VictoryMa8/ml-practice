099fe4e0476ce953a0e4a084758cb9096f12d2f5
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-01 18:54:59 -0600

    First commit of 2025

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import time
+
+currentTime = time.localtime();
+
 def square(x):
     return x * x
 
@@ -7,6 +11,8 @@ def main():
     num1 = input("Enter a number: ")
     result = square(int(num1))
     print("Greetings, " + name + ", " + num1 + " squared is " + str(result))
+    print("It is currently ", currentTime)
+
 
 if __name__ == "__main__":
     main()
