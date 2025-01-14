e81bfca4d167c8169ea980e603e8c1b5de72ec5d
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-14 10:45:56 -0600

    review

--- python_review/classes_objects.py
+++ python_review/classes_objects.py
@@ -9,20 +9,23 @@ class myClass:
 # use the class to create an object
 obj1 = myClass()
 
-# to understand the meaning of classes we have to understand the built-in __init__() function
 # all classes have a function called __init__(), which is always executed when the class is being initiated
-# use the __init__() function to assign values to object properties,
-# or other operations that are necessary to do when the object is being created
-
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+    # use the __init__() function to assign values to object properties,
+    # or other operations that are necessary to do when the object is being created
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    # the __str__() function controls what is returned when the class object is represented as a string
+    
+    def __str__(self):
+        return f"This person is {self.name}, who is {self.age} years old."
+    
 person1 = Person("Victory Ma", 21)
 
 def main():
-    print(person1.name, person1.age)
+    print(person1)
 
 if __name__ == "__main__":
     main()
--- python_review/lambda.py
+++ python_review/lambda.py
@@ -6,8 +6,18 @@ syntax: lambda <arguments> : <expression>
 
 example = lambda x : x + 10
 
+# template for a function with lambda
+def template(n):
+  return lambda a : a * n
+
+# create a new function from this template function
+# the argument passed through will be n
+doublingFunction = template(2)
+    # return a <- a * 2
+
 def main():
     print(example(5)) # prints 5 --> lambda expression --> 5 + 10 --> 15
+    print(doublingFunction(50))
 
 if __name__ == "__main__":
     main()
