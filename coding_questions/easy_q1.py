424e92ce8bd5934593a5e35ebb092274a41686d7
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-16 10:12:47 -0600

    eval exp tree

--- coding_questions/easy_q1.py
+++ coding_questions/easy_q1.py
@@ -313,6 +313,23 @@ def optimalFreelancing(jobs):
         numberOfDays -= 1
     return maxProfit
 
+# explaining ltr **
+def evaluateExpressionTree(tree):
+    if tree.value >= 0:
+        return tree.value
+        
+    leftValue = evaluateExpressionTree(tree.left)
+    rightValue = evaluateExpressionTree(tree.right)
+
+    if tree.value == -1:
+        return leftValue + rightValue
+    if tree.value == -2:
+        return leftValue - rightValue
+    if tree.value == -3:
+        return int(leftValue / rightValue)
+        
+    return leftValue * rightValue
+
 def main():
     print("Hello")
 
---
+++ coding_questions/medium_q2.py
@@ -0,0 +1,12 @@
+'''
+Medium Level Coding Questions (Part 2)
+
+
+
+'''
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
