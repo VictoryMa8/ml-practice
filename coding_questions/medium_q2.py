28273b987cc425c7c5cf4b769ea647ebd19a4486
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-18 22:42:24 -0600

    medium

--- coding_questions/medium_q2.py
+++ coding_questions/medium_q2.py
@@ -1,9 +1,21 @@
 '''
-Medium Level Coding Questions (Part 2)
+Medium & Hard Level Coding Questions (Part 2)
 
-
+1. Reverse Linked List
 
 '''
+
+# 1. Reverse Linked List
+def reverseLinkedList(head):
+    # 1 -> 5 -> 3 -> 7 -> 9
+    previousNode = None
+    currentNode = head # 1
+    while currentNode is not None:
+        nextNode = currentNode.next # nextNode = currentNode.next = 5
+        currentNode.next = previousNode # currentNode.next = None
+        previousNode = currentNode # previousNode = currentNode = 1
+        currentNode = nextNode # currentNode = nextNode = 5
+    return previousNode
 
 def main():
     print("Hello World!")
