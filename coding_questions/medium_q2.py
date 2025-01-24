b93ecdcd0234716727dfe842ff9b852858a6d4b5
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-01-24 16:46:27 -0600

    leven dist

--- coding_questions/medium_q2.py
+++ coding_questions/medium_q2.py
@@ -12,6 +12,7 @@ Medium & Hard Level Coding Questions (Part 2)
 9. Merge Sort
 10. Knapsack Problem
 11. Four Number Sum
+12. Levenshtein Distance
 
 '''
 
@@ -213,6 +214,19 @@ def fourNumberSum(array, targetSum):
                 else:
                     left += 1
     return result
+
+# 12. Levenshtein Distance
+def levenshteinDistance(str1, str2):
+    edits = [[x for x in range(len(str1) + 1)] for y in range(len(str2) + 1)]
+    for i in range(1, len(str2) + 1):
+        edits[i][0] = edits[i - 1][0] + 1
+    for i in range(1, len(str2) + 1):
+        for j in range(1, len(str1) + 1):
+            if str2[i - 1] == str1[j - 1]:
+                edits[i][j] = edits[i - 1][j - 1]
+            else:
+                edits[i][j] = 1 + min(edits[i - 1][j - 1], edits[i - 1][j], edits[i][j - 1])
+    return edits[-1][-1]
 
 def main():
     print("Hello World!")
