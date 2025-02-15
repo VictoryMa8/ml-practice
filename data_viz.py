8ab0876fade19190ac583fce690113e09983a28c
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-14 22:08:07 -0600

    stuff

---
+++ data_viz.py
@@ -0,0 +1,19 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+penguins = sns.load_dataset('penguins')
+
+penguins.plot(kind = "scatter",
+              x= "flipper_length_mm",
+              y = "body_mass_g")
+
+sns.lmplot(penguins,
+           x = 'flipper_length_mm',
+           y = 'bill_length_mm',
+           hue = 'island',
+)
+plt.title('Biscoe Penguins Have Big Feet and Bills, Dream Penguins.. Just Bills')
+plt.xlabel('Flipper Length (mm)')
+plt.ylabel('Bill Length (mm)')
+
+plt.show()
