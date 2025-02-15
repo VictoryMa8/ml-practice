2dc248c92410adb31a14c93f80e7fd6daf1f8c95
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-15 12:01:29 -0600

    stuff

--- data_viz.py
+++ data_viz.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 
 penguins = sns.load_dataset('penguins')
 
-penguins.plot(kind = "scatter",
-              x= "flipper_length_mm",
-              y = "body_mass_g")
+def main():
+    penguins.plot(kind = "scatter",
+                x= "flipper_length_mm",
+                y = "body_mass_g")
 
-sns.lmplot(penguins,
-           x = 'flipper_length_mm',
-           y = 'bill_length_mm',
-           hue = 'island',
-)
-plt.title('Biscoe Penguins Have Big Feet and Bills, Dream Penguins.. Just Bills')
-plt.xlabel('Flipper Length (mm)')
-plt.ylabel('Bill Length (mm)')
+    sns.lmplot(penguins,
+            x = 'flipper_length_mm',
+            y = 'bill_length_mm',
+            hue = 'island',
+    )
 
-plt.show()
+    plt.title('Biscoe Penguins Have Big Feet and Bills, Dream Penguins.. Just Bills')
+    plt.xlabel('Flipper Length (mm)')
+    plt.ylabel('Bill Length (mm)')
+    plt.show()
+
+if __name__ == "__main__":
+    main()
