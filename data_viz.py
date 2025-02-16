211d9237d79266159f6de4b410b076c6d49100f3
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-16 17:47:52 -0600

    data viz practice

--- data_viz.py
+++ data_viz.py
@@ -2,9 +2,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 penguins = sns.load_dataset('penguins')
-flights = sns.load_dataset('flights')
 
 def main():
+    '''
     sns.lmplot(penguins,
             x = 'flipper_length_mm',
             y = 'bill_length_mm',
@@ -14,9 +14,10 @@ def main():
     plt.xlabel('Flipper Length (mm)')
     plt.ylabel('Bill Length (mm)')
     plt.show()
+    '''
 
-    print(penguins.head())
-    print(penguins.info())
+    # print(penguins.head())
+    # print(penguins.info())
     print(penguins.describe())
 
 if __name__ == "__main__":
