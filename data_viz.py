7f1d8cafe4884be593e72d17e95702f9a25dbe0b
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-16 10:47:22 -0600

    new

--- data_viz.py
+++ data_viz.py
@@ -2,22 +2,22 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 penguins = sns.load_dataset('penguins')
+flights = sns.load_dataset('flights')
 
 def main():
-    penguins.plot(kind = "scatter",
-                x= "flipper_length_mm",
-                y = "body_mass_g")
-
     sns.lmplot(penguins,
             x = 'flipper_length_mm',
             y = 'bill_length_mm',
             hue = 'island',
     )
-
     plt.title('Biscoe Penguins Have Big Feet and Bills, Dream Penguins.. Just Bills')
     plt.xlabel('Flipper Length (mm)')
     plt.ylabel('Bill Length (mm)')
     plt.show()
+
+    print(penguins.head())
+    print(penguins.info())
+    print(penguins.describe())
 
 if __name__ == "__main__":
     main()
