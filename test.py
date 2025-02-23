53c0ea5ae5b17cce7ff056e13af65bff52c005c7
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-23 11:40:21 -0600

    lol fizzbuzz

--- test.py
+++ test.py
@@ -1,7 +1,19 @@
 x = 5 % 2
 
+def fizz_buzz():
+    for i in range(1, 100):
+        if i % 3 == 0 and i % 5 == 0:
+            print("Fizz Buzz!")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
 def main():
     print(x)
+    fizz_buzz()
 
 if __name__ == "__main__":
     main()
