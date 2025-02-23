8d1041c6673dad2f4d086a16a3138886ea1476d9
Victor <160278295+VictoryMa8@users.noreply.github.com>
2025-02-23 12:30:49 -0600

    this is silly

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 x = 5 % 2
 
-def fizz_buzz():
+def joe_mama():
     for i in range(1, 100):
         if i % 3 == 0 and i % 5 == 0:
-            print("Fizz Buzz!")
+            print("Joe Mama!")
         elif i % 3 == 0:
-            print("Fizz")
+            print("Joe")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Mama")
         else:
             print(i)
 
 def main():
     print(x)
-    fizz_buzz()
+    joe_mama()
 
 if __name__ == "__main__":
     main()
